2616d9dcd7a5e74132320d529967349ff3d76359
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-05-23 21:44:59 +0300

    add counter-reducers & store.ts

---
+++ src/Bll/store.ts
@@ -0,0 +1,6 @@
+import {combineReducers, createStore} from "redux";
+import {counterReducer} from "./counter-reducer";
+const rootReducer = combineReducers({
+  counter: counterReducer
+})
+export const store = createStore(rootReducer)
