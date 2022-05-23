b8a07525bce9d621b601ab27a69e361723a6d54f
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-05-23 14:12:56 +0300

    add changeCounterReducer.ts

---
+++ src/Reducers/changeCounterReducer.ts
@@ -0,0 +1,30 @@
+
+
+export const changeCounterReducer = (state: any, action: any) => {
+  switch (action.type){
+    case 'INCREASE_COUNT':
+      return {
+        ...state,
+        count: action.newCount
+      }
+    case 'RESET_COUNT':
+      return {
+        ...state,
+        count: 0
+      }
+    default:
+      return state
+  }
+}
+const increaseCountAC = (newCount: number) => {
+  return{
+    type: 'INCREASE_COUNT',
+    count:newCount
+  }
+}
+const resetCountAC = () => {
+  return{
+    type: 'INCREASE_COUNT',
+    count: 0
+  }
+}
